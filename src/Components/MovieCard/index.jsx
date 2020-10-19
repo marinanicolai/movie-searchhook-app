4131d6e24361a97a3d6a83a4a92bdbf7467b4618
Marina Nicolai <nicolaimarina@gmail.com>
2020-10-19 14:58:51 -0400

    first commit

---
+++ src/Components/MovieCard/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+function MovieCard() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Movie search app using hooks</p>
+      </header>
+    </div>
+  );
+}
+
+export default MovieCard;
---
+++ src/Components/MovieList/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import 
+
+function MovieList() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Movie search app using hooks</p>
+      </header>
+    </div>
+  );
+}
+
+export default MovieList;
